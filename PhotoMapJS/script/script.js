bd1806409746b7ce79fdce11de1078e453406a6a
Luca Regazzi <luca.regazzi@gmail.com>
2015-09-26 14:01:53 +0200

    updated PhotoMapJS

--- PhotoMapJS/script/script.js
+++ PhotoMapJS/script/script.js
@@ -2,6 +2,14 @@ var allPhotos = [];
 var directory = document.getElementById("directoryinput");
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
+var examplePhotos = [ "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Atomic%20Dome.JPG",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Fushimi%20Inari.JPG",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Great%20buddha.JPG",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Itsukuschima.jpg",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Kinkaku-Ji.jpg",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Miyajima.jpg",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Osaka.jpg",
+                      "https://dl.dropboxusercontent.com/u/5692098/PhotoMapJS%20Test/Tokyo%20Tower.jpg"];
 
 //Sidebar Handling
 $('#arrow').click(function(){
@@ -26,6 +34,10 @@ $('#arrow').click(function(){
 
 dbutton.addEventListener("click", function() {
   document.getElementById('directoryinput').click()
+}, false);
+
+ebutton.addEventListener("click", function() {
+  readExamplePhotos();
 }, false);
 
 var markers = new L.MarkerClusterGroup();
@@ -68,6 +80,7 @@ var handleFile = function (event) {
 
             allPhotos.push({
               photo: single_file,
+              type : 'l',
               realWidth : x,
               realHeight : y,
               height: Math.floor(newsize.height) ,
@@ -106,21 +119,81 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 
 function onMarkerClick(e){
   var j = e.target.options.alt;
+  var photo = allPhotos[j];
+  if(photo.type=='l'){
+    var reader = new FileReader();
 
-  var reader = new FileReader();
+    reader.onload = function(event){
 
-  reader.onload = function(event){
+      var arrayBufferView = new Uint8Array( event.target.result );
+      var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL( blob );
+      var popup = L.popup({maxWidth:windowWidth*0.80})
+      .setLatLng(photo.latlng)
+      .setContent('<img src="'+imageUrl+'" height="'+photo.height+'" width="'+photo.width+'"/>')
+      .openOn(map);
+    };
 
-    var arrayBufferView = new Uint8Array( event.target.result );
-    var blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
-    var urlCreator = window.URL || window.webkitURL;
-    var imageUrl = urlCreator.createObjectURL( blob );
-    var popup = L.popup({maxWidth:windowWidth*0.80})
-    .setLatLng(allPhotos[j].latlng)
-    .setContent('<img src="'+imageUrl+'" height="'+allPhotos[j].height+'" width="'+allPhotos[j].width+'"/>')
-    .openOn(map);
-  };
+    reader.readAsArrayBuffer(photo.photo);
+  }else if(photo.type=='r'){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', photo.photo, true);
+    xhr.responseType = 'blob';
 
-  reader.readAsArrayBuffer(allPhotos[j].photo);
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        var blob = this.response;
+        var urlCreator = window.URL || window.webkitURL;
+        var imageUrl = urlCreator.createObjectURL( blob );
+        var popup = L.popup({maxWidth:windowWidth*0.80})
+        .setLatLng(photo.latlng)
+        .setContent('<img src="'+imageUrl+'" height="'+photo.height+'" width="'+photo.width+'"/>')
+        .openOn(map);
+      }
+    }
+    xhr.send();
+  }
+  
+
+}
+
+function readExamplePhotos(){
+  $.each(examplePhotos,function(index,url){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        exif = new ExifReader();
+
+        exif.load(this.response);
+        exif.deleteTag('MakerNote');
+
+        tags = exif.getAllTags();
+        if(typeof tags['GPSLatitude'] != 'undefined' && typeof tags['GPSLongitude'] != 'undefined'){
+          var x = typeof(tags['PixelXDimension']) != 'undefined' ? tags['PixelXDimension'].value : tags['ImageWidth'].value;
+          var y = typeof(tags['PixelYDimension']) != 'undefined' ? tags['PixelYDimension'].value : tags['ImageLength'].value;
+          var newsize = calculateAspectRatioFit(x,y,windowWidth*0.79,500);
+
+          allPhotos.push({
+              photo: url,
+              type: 'r',
+              realWidth : x,
+              realHeight : y,
+              height: Math.floor(newsize.height) ,
+              width: Math.floor(newsize.width),
+              latlng : [tags['GPSLatitude'].description,tags['GPSLongitude'].description]
+            });
+
+            var marker = L.marker([tags['GPSLatitude'].description,tags['GPSLongitude'].description],{alt : allPhotos.length-1});
+            marker.on('click', onMarkerClick);
+            markers.addLayer(marker);
+        }
+      }
+    }
+    xhr.send();
+  });
 
 }
