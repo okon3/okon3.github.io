fb3a43b73f2422f61b0c506d1854aecb0ab5262f
Luca Regazzi <luca.regazzi@gmail.com>
2016-11-18 23:25:14 +0100

    TheSpaceBologna

--- TheSpaceBologna/js/app.js
+++ TheSpaceBologna/js/app.js
@@ -10,8 +10,21 @@ weekday[6] = "Sabato";
 //Startup
 (function($){
     $(function(){
-        $('.button-collapse').sideNav();
+        //$('.button-collapse').sideNav();
         setDays();
+
+        $("#app").swipe( {
+        swipeLeft:function(event, direction, distance, duration, fingerCount) {
+            var currentDay = parseInt($('.tabs a.active').attr('href').substr(-1));
+            $('ul.tabs').tabs('select_tab', 'container-'+(++currentDay));
+        },
+        swipeRight:function(event, direction, distance, duration, fingerCount) {
+            var currentDay = parseInt($('.tabs a.active').attr('href').substr(-1));
+            $('ul.tabs').tabs('select_tab', 'container-'+(--currentDay));
+        },
+        threshold: 0
+      });
+
         loadFilms();
     });
 })(jQuery);
